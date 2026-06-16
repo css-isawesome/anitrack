e5b4abc99a365c01212e01f82dd8136882977a9d
Magda Dan <s175282@student.uken.krakow.pl>
2026-06-16 17:22:18 +0200

    poprawka api + ekrany

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,15 @@
 import 'dart:convert';
+import 'package:anitrack/models/info_anime.dart';
 import 'package:http/http.dart' as http;
 import '../models/top_anime.dart';
-class TaskApiService {
+
+class ApiService {
   // linkacz do jikan api
   static const String baseUrl = "https://api.jikan.moe/v4";
 
   // pobieranie top anime do wyswietlenia na stronie glownej
   static Future<List<TopAnime>> fetchTopAnime() async {
-    final response = await http.get(
-      Uri.parse("$baseUrl/top/anime?sfw"),
-    );
+    final response = await http.get(Uri.parse("$baseUrl/top/anime?sfw"));
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
       final List animes = data["data"];
@@ -25,21 +25,35 @@ class TaskApiService {
     }
   }
 
-  // pobieranie szczegolowych informacji o konkretnym anime
-  static Future<List<TopAnime>> fetchInfoAnime(int id) async {
-    final response = await http.get(
-      Uri.parse("$baseUrl/anime/$id/full"),
-    );
+  // pobieranie szczegolowych informacji o konkretnym POJEDYNCZYM anime
+  static Future<InfoAnime> fetchInfoAnime(int id) async {
+    final response = await http.get(Uri.parse("$baseUrl/anime/$id/full"));
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      final List animes = data["data"];
-      return animes.map((anime) {
-        return TopAnime(
+      final Map<String, dynamic> anime = data["data"]; // pojedynczy obiekt anime w polu data
+
+      // pobiera liste gatunkow (bo moze byc ich kilka) i potem laczy a lancuch oddzielajac je przecinkami
+      final List genresList = anime["genres"] ?? [];
+      final String genresString = genresList.map((g) => g["name"]).join(", ");
+
+      final List demoList = anime["demographics"] ?? [];
+      final String demoString = demoList.map((d) => d["name"]).join(", ");
+
+      final List studiosList = anime["studios"] ?? [];
+      final String studiosString = studiosList.map((s) => s["name"]).join(", ");
+
+        return InfoAnime(
           id: anime["mal_id"],
           engTitle: anime["title_english"] ?? anime["title"] ?? "No Title",
           img: anime["images"]["jpg"]["image_url"],
+          jpTitle: anime["title_japanese"] ?? "No Title",
+          status: anime["status"] ?? "Unknown",
+          aired: anime["aired"]["string"] ?? "Unknown",
+          demographic: demoString.isEmpty ? "None" : demoString,
+          genre: genresString.isEmpty ? "None" : genresString,
+          score: (anime["score"] ?? 0.0).toDouble(), //
+          studios: studiosString.isEmpty ? "Unknown" : studiosString
         );
-      }).toList();
     } else {
       throw Exception("Błąd pobierania danych"); // blad jezeli kod inny niz 200
     }
